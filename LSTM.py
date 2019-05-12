f9fc5b19013099dd5e07557850a8fdc5c0d5ee60
EdgarCeh <eduardo.ceh@hotmail.com>
2019-05-12 11:46:02 -0600

    Running with 2 input features and 2 output features

--- LSTM.py
+++ LSTM.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 import torch.nn as nn
@@ -9,21 +10,129 @@ import sys
 from Common import *
 import Utilities as utils
 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 df = utils.get_all_data(FILENAME_ALL_DATA)
 df = utils.get_target_data(df, TARGET_ID)
 
-x_data = [[x] for x in list(df[X].values)]
-y_data = [[y] for y in list(df[Y].values)]
+print(df.head(seq_length+1))
 
-x_scale, x_data = utils.scale_data(x_data)
-y_scale, y_data = utils.scale_data(y_data)
+data = df[[X,Y]].values.tolist()
+print(data)
+print(np.asarray(data).shape)
 
+sc, scaled_data = utils.scale_data(data)
+print(scaled_data[:seq_length+1])
+print(np.asarray(scaled_data).shape)
+print(type(scaled_data))
 #utils.plot_trajectory(y_data)
-utils.plot_trajectory((x_data, y_data))
+utils.plot_trajectory(scaled_data)
 
-sequences, targets = utils.sliding_windows(x_data, seq_length)
-print("Sequence size:", sequences.shape)
-print("target size:", targets.shape)
+# Only working with one data here
+sequences, targets = utils.sliding_windows(scaled_data, seq_length)
+print("Sequences size:", sequences.shape)
+print("Targets size:", targets.shape)
+
+print("Sequences:")
 print(sequences[0])
+print("Targets:")
 print(targets[0])
 
+real_seq = utils.inverse_scale_data(sc, sequences[0])
+print(real_seq)
+real_target = utils.inverse_scale_data(sc, [targets[0]])
+print(real_target)
+
+
+train_size = int(len(targets) * TRAIN_PCT)
+test_size = len(targets) - train_size
+print("Train size:", train_size)
+print("Test size:", test_size)
+
+train_X = sequences[0:train_size]
+test_X = sequences[train_size:len(sequences)]
+
+train_Y = targets[0:train_size]
+test_Y = targets[train_size:len(targets)]
+
+trainX = torch.Tensor(train_X)
+trainX = Variable(trainX).to(device)
+
+testX = torch.Tensor(test_X)
+testX = Variable(testX).to(device)
+
+trainY = torch.Tensor(train_Y)
+trainY = Variable(trainY).to(device)
+
+testY = torch.Tensor(test_Y)
+testY = Variable(testY).to(device)
+
+print("Train X size:", trainX.size())
+print("Train y size:", trainY.size())
+
+print("Test X size:", testX.size())
+print("Test Y size:", testX.size())
+
+class LSTM(nn.Module):
+
+    def __init__(self, output_size, features, hidden_size, num_layers):
+        super(LSTM, self).__init__()
+        self.num_classes = output_size
+        self.num_layers = num_layers
+        self.input_size = features
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
+
+        self.lstm = nn.LSTM(input_size=features, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True)
+
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device)
+        ##print("h_0 shape:",h_0.size())
+
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device)
+        ##print("c_0 shape:", c_0.size())
+
+        # Propagate input through LSTM
+        ula, (h_out, _) = self.lstm(x, (h_0, c_0))
+        h_out = h_out.view(-1, self.hidden_size)
+        ##print("h_out shape:", h_out.size())
+
+        out = self.fc(h_out)
+        ##print("out shape:", out.size())
+
+        return out
+
+
+lstm = LSTM(output_size, features, hidden_size, num_layers).to(device)
+criterion = torch.nn.MSELoss()    # mean-squared error for regression
+optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    outputs = lstm(trainX)
+    #print(outputs)
+    #print(outputs.size())
+    #print(train_Y)
+    optimizer.zero_grad()
+    # obtain the loss function
+    loss = criterion(outputs, trainY)
+    loss.backward()
+    optimizer.step()
+    print("Epoch: %d, loss: %1.5f" % (epoch, loss.data))
+
+
+lstm.eval()
+test_predict = lstm(testX)
+
+test_predict = utils.inverse_scale_data(sc, test_predict.data.cpu().numpy())
+testY = utils.inverse_scale_data(sc, testY.data.cpu().numpy())
+print(testY.shape)
+print(test_predict.shape)
+
+
+utils.plot_compre_trajectories(testY, test_predict)
