5eae6cf59ad13a130cf750f49a0994de9815b2cd
EdgarCeh <eduardo.ceh@hotmail.com>
2019-05-12 11:44:59 -0600

    Added TRAIN_PCT

--- Common.py
+++ Common.py
@@ -8,13 +8,16 @@ TARGET_ID = 2
 X = "XLocation"
 Y = "YLocation"
 
+## Train-test split
+TRAIN_PCT = 0.999
+
 
 # LSTM Hyper-parameters
-seq_length = 7
+seq_length = 20
 learning_rate = 0.01
-input_size = 1
-hidden_size = 5
-num_classes = 1
+features = 2      #input_size
+hidden_size = 50
+output_size = 2     #output
 num_layers = 1
 num_epochs = 500
 
