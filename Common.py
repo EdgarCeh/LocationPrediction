84f9e74b229b164d8a87ae1d346b8a289411fbc2
EdgarCeh <eduardo.ceh@hotmail.com>
2019-05-13 16:14:39 -0600

    Data concatenation variables

--- Common.py
+++ Common.py
@@ -9,8 +9,9 @@ X = "XLocation"
 Y = "YLocation"
 
 ## Train-test split
-TRAIN_PCT = 0.999
-
+TRAIN_PCT = 0.7#0.999
+CONCAT = True      # To repeat the Data
+N_TIMES = 3     # The number of Data repetitions
 
 # LSTM Hyper-parameters
 seq_length = 20
@@ -18,6 +19,6 @@ learning_rate = 0.01
 features = 2      #input_size
 hidden_size = 50
 output_size = 2     #output
-num_layers = 1
+num_layers = 2
 num_epochs = 500
 
